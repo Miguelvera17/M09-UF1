494d367333aa18c69fc4fa3dc43ea06958e8bf30
Miguel Vera <2023_miguel.vera@iticbcn.cat>
2024-10-23 20:50:36 +0200

    falta

---
+++ 06_Factory/src/iticbcn/xifratge/XifradorMonoalfabetic.java
@@ -0,0 +1,72 @@
+package iticbcn.xifratge;
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class XifradorMonoalfabetic {
+    
+    public final char[] alfabet = "aàábcçdeèéfghiíïjklmnñoóòpqrstuúüvwxyz".toCharArray();
+    public char[] alfabetPermuta = permutaAlfabet(alfabet);
+
+    public static char[] permutaAlfabet(char[] alfabet) {
+        ArrayList<Character> abclist = new ArrayList<>();
+        for (char i : alfabet) {
+            abclist.add(i);
+        }
+        Collections.shuffle(abclist);
+        char [] abcPermuta = new char[abclist.size()];
+        for (int i = 0; i < abclist.size(); i++) {
+            abcPermuta[i] = abclist.get(i);
+        }
+        return abcPermuta;
+    }
+
+    public String xifraMonoAlfa (String cadena) {
+        String s = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char letra = cadena.charAt(i);
+            if (esLetra(letra)) {
+                if (Character.isUpperCase(letra)) {
+                    letra = Character.toLowerCase(letra);
+                    for (int j = 0; j < alfabet.length; j++) {
+                        if (letra == alfabet[j]) {
+                            s = s + Character.toUpperCase(alfabetPermuta[j]);
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < alfabet.length; j++) {
+                        if (letra == alfabet[j]) {
+                            s = s + alfabetPermuta[j];
+                        }
+                    }
+                } 
+            } else {s = s + letra;}
+        } return s;
+    } 
+    
+    public String desxifraMonoAlfa (String cadena) {
+        String s = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char letra = cadena.charAt(i);
+            if (esLetra((letra))) {
+                if (Character.isUpperCase(letra)) {
+                    for (int j = 0; j < alfabetPermuta.length; j++) {
+                        if (Character.toLowerCase(letra) ==alfabetPermuta[j]) {
+                            s = s + Character.toUpperCase(alfabet[j]);
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < alfabetPermuta.length; j++) {
+                        if (letra ==alfabetPermuta[j]) {
+                            s = s + alfabet[j];
+                        }
+                    }
+                }
+            } else {s = s + letra;}
+        } return s;
+    }
+
+    public boolean esLetra (char letra) {
+        if (Character.isLetter(letra)) {return true;}
+        else {return false;}
+    }
+}
--- 06_Factory/src/iticbcn/xifratge/XifradorRotX.java
+++ 06_Factory/src/iticbcn/xifratge/XifradorRotX.java
@@ -1,38 +1,16 @@
-import java.util.Scanner;
+package iticbcn.xifratge;
 
-public class Rot13 {
+public class XifradorRotX implements Xifrador{
 
-    public static final String ABC = "abcdefghijklmnopqrstuvwxyz";
-    public static final String ABCMAY = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public final String ABC = "abcdefghijklmnopqrstuvwxyz";
+    public final String ABCMAY = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public static void main (String[] args){
-        Scanner scann = new Scanner(System.in);
-        Scanner scann2 = new Scanner(System.in);
-        System.out.println("\n Introduzca la contraseña");
-        String cadena = scann.nextLine();
-        if (cadena.isEmpty() || cadena.isBlank()) {
-            System.out.println("Error, debe introducir una contraseña");
-        } else {
-            System.out.println("\n Indique si desea \"cifrar\" o \"descifrar\"");
-            String accion = scann2.nextLine();
-            if (accion.equals("cifrar")) {
-                System.out.println("\n Contraseña cifrada: " + xifraRot13(cadena));
-            } if (accion.equals("descifrar")) {
-                System.out.println("\n Contraseña descifrada: " + desxifraRot13(cadena));
-            } //if(!accion.equals("cifrar") || !accion.equals("descifrar")) {
-                //System.out.println("No es una opción correcta");
-            //}
-        }
-        scann.close();
-        scann2.close();
-    }
-
-    public static boolean esLetra (char letra) {
+    public boolean esLetra (char letra) {
         if (Character.isLetter(letra)) {return true;}
         else {return false;}
     }
 
-    public static String xifraRot13 (String cadena) {
+    public String xifraRotX (String cadena) {
         String s = "";
         for (int i = 0; i < cadena.length(); i++) {
             char letra = cadena.charAt(i);
@@ -51,7 +29,7 @@ public class Rot13 {
         } return s;
     } 
     
-    public static String desxifraRot13 (String cadena) {
+    public String desxifraRotX (String cadena) {
         String s = "";
         for (int i = 0; i < cadena.length(); i++) {
             char letra = cadena.charAt(i);
